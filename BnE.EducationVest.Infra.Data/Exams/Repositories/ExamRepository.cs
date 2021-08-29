3469fa2cfad8932195807682e9bd98b7935fc328
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2021-08-28 21:28:08 -0300

    Temp adding -3 hours to get available exams

--- BnE.EducationVest.Infra.Data/Exams/Repositories/ExamRepository.cs
+++ BnE.EducationVest.Infra.Data/Exams/Repositories/ExamRepository.cs
@@ -19,7 +19,7 @@ namespace BnE.EducationVest.Infra.Data.Exams.Repositories
 
         public async Task<List<Exam>> GetAvailableExamsByUser(Guid userId)
         {
-            var actualDate = DateTime.Now;
+            var actualDate = DateTime.Now.AddHours(-3);
             var exams = await _db
                 .Include(x => x.Periods)
                 .Include(x => x.Finalizeds.Where(x => x.UserId == userId))
